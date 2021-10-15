5a5df3ad96b83e2708a89e1c6f159d9c06132e5e
ukalkan <ugur.kalkan@msuyazilim.com>
2021-10-15 20:29:25 +0300

    Added CORS, enable all

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace EBO.CodingTask.API
 {
@@ -10,6 +11,8 @@ namespace EBO.CodingTask.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*"); //allow all
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
