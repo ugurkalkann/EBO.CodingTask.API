641a8bb2d046fc0783259262bf51369b6ebc675e
ukalkan <ugur.kalkan@msuyazilim.com>
2021-10-15 20:16:23 +0300

    Products operations added

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using EBO.CodingTask.API.Data;
+using EBO.CodingTask.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EBO.CodingTask.API.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private static readonly IProductService _productRepository = new ProductService();
+
+        public IEnumerable<Product> Get()
+        {
+            return _productRepository.GetAllProducts();
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _productRepository.GetProduct(id);
+        }
+
+        [HttpPost]
+        public Product OrderProduct([FromBody] OrderProductRequest request)
+        {
+            return _productRepository.OrderProduct(request);
+        }
+    }
+}
